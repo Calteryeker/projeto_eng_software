18ff4f08f30757db7375e7138f6d846e82c24be2
HyanBatista <hyanbatista42@gmail.com>
2021-06-26 18:59:59 -0300

    chore: add tests for expense controller

--- src/tests/controllers/ControladorDespesaTest.java
+++ src/tests/controllers/ControladorDespesaTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +13,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import controllers.ControladorDespesa;
+import dao.impl.exceptions.CategoriaNulaException;
 import dao.impl.exceptions.DadosNaoPreenchidosException;
+import dao.impl.exceptions.DespesaNaoEncontradaException;
 import model.Categoria;
 import model.Despesa;
 
@@ -59,6 +62,54 @@ public class ControladorDespesaTest {
         });
 
         assertEquals("Os dados não foram preenchidos corretamente", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Edit expense should work")
+    public void testEditExpense() throws DadosNaoPreenchidosException, DespesaNaoEncontradaException {
+        Despesa expense = controller.criarDespesa("Viagem para Iputinga", 233d, date, category);
+        Despesa altExpense = controller.alterarDespesa("Viagem para o DETRAN", expense.getOrdem(), expense.getValor(),
+                expense.getData_criacao(), expense.getCategoria());
+        assertEquals("Viagem para o DETRAN", altExpense.getNome());
+    }
+
+    @Test
+    @DisplayName("Edit expense with an expense that doesn't exist")
+    public void testEditWithNonExistentExpenseFails() throws DadosNaoPreenchidosException {
+        Despesa expense = controller.criarDespesa("Viagem para Iputinga", 233d, date, category);
+        Exception exception = assertThrows(DespesaNaoEncontradaException.class, () -> {
+            controller.alterarDespesa("Viagem para o DETRAN", 12341234, expense.getValor(),
+                expense.getData_criacao(), expense.getCategoria());
+        });
+        assertEquals("Despesa não encontrada!!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Remove expense should work")
+    public void testRevomeExpenseFails() throws DadosNaoPreenchidosException, DespesaNaoEncontradaException {
+        Despesa expense = controller.criarDespesa("Viagem para Iputinga", 233d, date, category);
+        Despesa delExpense = controller.removerDespesa(expense.getOrdem());
+        assertThrows(DespesaNaoEncontradaException.class, () -> {
+            controller.removerDespesa(delExpense.getOrdem());
+        });
+    }
+
+    @Test
+    @DisplayName("Show expenses per category should work")
+    public void testShowExpensesPerCategorySucceeds() throws DadosNaoPreenchidosException, CategoriaNulaException {
+        Despesa expense = controller.criarDespesa("Viagem para Iputinga", 233d, date, category);
+        List<Despesa> expenses = controller.visualizarDespesasPorCategoria(category);
+        assertEquals(expense.getNome(), expenses.get(0).getNome());
+    }
+
+    @Test
+    @DisplayName("Show expenses per category with missing data should throw CategoriaNulaException")
+    public void testShowExpensesWithMissingDataFails() {
+        Exception exception = assertThrows(CategoriaNulaException.class, () -> {
+            controller.visualizarDespesasPorCategoria(null);
+        });
+
+        assertEquals("Categoria Nula!!", exception.getMessage());
     }
 
     @AfterEach
