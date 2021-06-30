7233bee4cf9dfd4c3405669698fa2e97a6778f8b
HyanBatista <hyanbatista42@gmail.com>
2021-06-30 20:24:16 -0300

    implement DadosPersistentes methods

--- src/dao/IDadosPersistentes.java
+++ src/dao/IDadosPersistentes.java
@@ -1,23 +1,28 @@
 package dao;
 
 import dao.impl.exceptions.UsuarioJaCadastradoException;
+import dao.impl.exceptions.UsuarioNaoEncontradoException;
+import model.Categoria;
+import model.Despesa;
+import model.Meta;
 import model.Usuario;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IDadosPersistentes {
 
-    void recuperarMetasUsuario();
+    List<Meta> recuperarMetasUsuario(String login);
 
-    void recuperarDespesasUsuario();
+    List<Despesa> recuperarDespesasUsuario(String login);
 
-    void salvarDadosUsuario();
+    void salvarDadosUsuario(String login);
 
-    void cadastrarNovoUsuario(String nome, String login, String senha) throws UsuarioJaCadastradoException;
+    Usuario cadastrarNovoUsuario(String nome, String login, String senha) throws UsuarioJaCadastradoException;
 
-    void recuperarCategorias();
+    Map<String, Categoria> recuperarCategorias(String login);
 
-    void removerUsuario();
+    Usuario removerUsuario(String login) throws UsuarioNaoEncontradoException;
 
     List<Usuario> getUsuarios();
 
