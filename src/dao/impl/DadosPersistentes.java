f460066b91163b5e0786a66ca4acf1b2424b6695
Cláudio Araújo <claudiomarciofilho@hotmail.com>
2021-07-01 16:33:13 -0300

    update DadosPersistentes

--- src/dao/impl/DadosPersistentes.java
+++ src/dao/impl/DadosPersistentes.java
@@ -76,7 +76,7 @@ public class DadosPersistentes implements IDadosPersistentes {
         if (usuarioDel != null) {
             usuarios.remove(usuarioDel);
         } else {
-            throw new UsuarioNaoEncontradoException("Despesa não encontrada!!");
+            throw new UsuarioNaoEncontradoException("Usuario não encontrado!!");
         }
 
         FileUtilRepository.saveFile(usuarios, path);
@@ -92,7 +92,7 @@ public class DadosPersistentes implements IDadosPersistentes {
 
     public Usuario buscarUsuario(String login) {
         for (Usuario usuario : usuarios) {
-            if (usuario.getLogin() == login) {
+            if (usuario.getLogin().equals(login)) {
                 return usuario;
             }
         }
